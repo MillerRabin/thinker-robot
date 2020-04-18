564b710bb9f3b245c1e0185ddf8a94d6a426921c
Vladimir Aseev <nemesisv@mail.ru>
2020-04-18 23:00:30 +0300

    Added negine control

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,154 @@
 
 #include "esp32-hal-ledc.h"
 #include <Arduino.h>
+#include <WiFi.h>
+#include<vector>
 
-void setup() {
+const char* ssid = "DragonSlayer";
+const char* password = "ifyouwanttohave";
+
+WiFiServer server(80);
+
+void WiFIConnect() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }  
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+
+void send(WiFiClient client, String code, String text) {
+   client.println("HTTP/1.1 " + code + " OK");
+   client.println("Content-type:text/html");
+   client.println("Connection: close");
+   client.println();   
+   client.println(text);    
+}
+
+bool isNewLine(char symbol) {
+   return ((symbol == '\n') || (symbol == '\r'));
+}
+
+
+std::vector<String> parseRequest(String clientData) {
+   unsigned int ln = clientData.length();   
+   std::vector<String> pr;   
+   unsigned int sIndex = 0;
+   for (unsigned int i = 0; i < ln; i++) {
+      const char smb = clientData[i];
+      if (isNewLine(smb)) continue;
+      if ((smb == ' ') || (i == ln - 1)) {
+         if (sIndex != i)
+            pr.push_back(clientData.substring(sIndex, i));
+         sIndex = i + 1;
+      }      
+   }
+   return pr;
+}
+
+String getQuery(String URL) {
+   int pos = URL.indexOf("?");
+   if (pos == -1) return "";   
+   return URL.substring(pos+1, URL.length());
+}
+
+std::vector<unsigned long> getEngineParameters(String query) {
+   std::vector<unsigned long> res;
+   int pos = query.indexOf(":");
+   if (pos == -1) return res;
+   String fp = query.substring(0, pos);
+   String sp = query.substring(pos+1, query.length());
+   res.push_back(fp.toInt());
+   res.push_back(sp.toInt());   
+   return res;
+}
+
+void setEngine(unsigned long engine, unsigned long angle) {
+   ledcWrite(engine, angle);
+}
+
+void route(WiFiClient client, String clientData) {
+   std::vector<String> pr = parseRequest(clientData);
+   if (pr.size() < 2) {
+      send(client, "400", "Invalid request");
+      return;
+   }
+   String url = pr[1];   
+   String query = getQuery(url);
+   std::vector<unsigned long> ep;
+   try {
+      ep = getEngineParameters(query);
+      if (ep.size() < 2) {
+         send(client, "400", "Invalid query");
+         return;
+      }
+   } catch (...){
+      send(client, "400", "Invalid query");
+      return;
+   }
+
+   unsigned long engine = ep[0];
+   unsigned long value = ep[1];
+
+   if ((engine < 1) || (engine > 4)) {
+      send(client, "400", "Invalid engine number");
+      return;
+   }
+
+   if (value < COUNT_LOW) {
+      send(client, "400", "Value too low");
+      return;
+   }
+
+   if (value > COUNT_HIGH) {
+      send(client, "400", "Value too high");
+      return;
+   }
+
+   setEngine(engine, value);
+   send(client, "200", "OK");
+}
+
+
+String readClient(WiFiClient client) {
+   String header = "";
+   String currentLine = "";
+   while (client.connected()) {    
+      if (!client.available()) return header;
+      char c = client.read();
+      header += c;
+      if (c == '\n') {            
+         if (currentLine.length() == 0) {            
+            return header;
+         }
+         currentLine = "";
+         continue;
+      }      
+      if (c == '\r') 
+         continue;
+      currentLine += c;      
+   }
+   return header;
+}
+
+void dispatchClient() {
+   WiFiClient client = server.available();
+   if (!client) return;   
+   String clientData = readClient(client);
+   route(client, clientData);   
+   client.stop();
+}
+
+
+void setupEngines() {
    ledcSetup(1, 50, TIMER_WIDTH); // channel 1, 50 Hz, 16-bit width
    ledcAttachPin(22, 1);   // GPIO 22 assigned to channel 1
    
@@ -20,38 +166,13 @@ void setup() {
    ledcAttachPin(18, 4);   // GPIO 18 assigned to channel 4
 }
 
+void setup() {
+   setupEngines();
+   Serial.begin(115200);
+   WiFIConnect();
+   server.begin();
+}
+
 void loop() {
-  ledcWrite(1, COUNT_LOW);
-  delay(2000);
-  ledcWrite(1, COUNT_LOW + 700);
-  delay(2000);
-/*   for (int i=COUNT_HIGH ; i < COUNT_LOW ; i=i-100)
-   {
-      ledcWrite(1, i);       // sweep servo 1
-      delay(200);
-   }
-
-   for (int i=COUNT_LOW ; i < COUNT_HIGH ; i=i+100)
-   {
-      ledcWrite(1, i);       // sweep servo 1
-      delay(200);
-   }*/
-    
-   /*for (int i=COUNT_LOW ; i < COUNT_HIGH ; i=i+100)
-   {
-      ledcWrite(2, i);       // sweep servo 2
-      delay(200);
-   }
-
-   for (int i=COUNT_LOW ; i < COUNT_HIGH ; i=i+100)
-   {
-      ledcWrite(3, i);       // sweep the servo
-      delay(200);
-   }
-
-   for (int i=COUNT_LOW ; i < COUNT_HIGH ; i=i+100)
-    {
-      ledcWrite(4, i);       // sweep the servo
-      delay(200);
-    }*/
+   dispatchClient();
 }
